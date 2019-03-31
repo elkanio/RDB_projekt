958c22db2b9560427077d9891d1f624eb9870b4c
Filip_Kratochvil <filip.f.kratochvil@seznam.cz>
2019-03-31 19:56:57 +0200

    watermarking struct

---
+++ Public_Transportation/src/main/java/cz/tul/fm/public_transportation_database/WaterMarking.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cz.tul.fm.public_transportation_database;
+
+/**
+ *
+ * @author FilipKrat
+ */
+public class WaterMarking {
+    public void makeWatermark(){
+        
+    }
+    public void removeWatermark(){
+        
+    }
+    public boolean compareData(){
+        boolean isSame = true;
+        
+        return isSame;
+    }
+}
